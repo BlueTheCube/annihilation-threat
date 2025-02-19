2163eecf397bc1125fd3cc2455e5beb3d5afbd3b
Cubical box <67639725+BlueTheCube@users.noreply.github.com>
2025-02-19 16:25:27 +0800

    ayay

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,33 @@
 Events.on(ClientLoadEvent, () => {
+    Object.assign(Difficulty.casual, {
+        enemyHealthMultiplier: 1.5,
+        enemySpawnMultiplier: 2,
+        waveTimeMultiplier: 0.6
+    });
     
+    Object.assign(Difficulty.easy, {
+        enemyHealthMultiplier: 2,
+        enemySpawnMultiplier: 2.5,
+        waveTimeMultiplier: 0.6
+    });
+    
+    Object.assign(Difficulty.normal, {
+        enemyHealthMultiplier: 2.5,
+        enemySpawnMultiplier: 2.8,
+        waveTimeMultiplier: 0.6
+    });
+    
+    Object.assign(Difficulty.hard, {
+        enemyHealthMultiplier: 3.5,
+        enemySpawnMultiplier: 3.5,
+        waveTimeMultiplier: 0.6
+    });
+    
+    Object.assign(Difficulty.eradication, {
+        enemyHealthMultiplier: 5,
+        enemySpawnMultiplier: 5,
+        waveTimeMultiplier: 0.6
+    });
 });
 
 Object.assign(StatusEffects.boss, {

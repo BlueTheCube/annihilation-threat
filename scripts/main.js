b3fc16310e373ac011a5fa80057f7b10831532b9
Cubical box <67639725+BlueTheCube@users.noreply.github.com>
2025-02-19 20:21:27 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 Events.on(ClientLoadEvent, () => {
-    Color.scarlet = Color.valueOf("c74fff")
+    Pal.health = Color.valueOf("c74fff")
     
     Object.assign(Difficulty.casual, {
         enemyHealthMultiplier: 1.5,
